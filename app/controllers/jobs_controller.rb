8929be14e26461f86901a7d932ef53c2a000c91e
Andre Benjamim <andre@debian>
2017-01-21 18:19:59 -0200

    Add feature create jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -7,4 +7,14 @@ class JobsController < ApplicationController
   def show
     @job = Job.find(params[:id])
   end
+
+  def new
+  	@job = Job.new
+  end
+
+  def create
+  	@job = Job.create(params.require(:job).permit(:title, :location, :description, :company, :category))
+
+  	redirect_to @job
+  end
 end
